411b572c07ff548b2b74998dee3e2ee0409aa2b6
Victor Trakhtenberg <victortr75@gmail.com>
2013-09-11 10:45:54 +0300

    Test and implement various parameters calculations

--- src/main/groovy/com/swagger/docgen/handlers/ParametersHandler.groovy
+++ src/main/groovy/com/swagger/docgen/handlers/ParametersHandler.groovy
@@ -1,11 +1,32 @@
 package com.swagger.docgen.handlers
 
+import com.wordnik.swagger.annotations.ApiParam
+
 import java.lang.annotation.Annotation
 
 class ParametersHandler {
 
+    def handleParameters(Annotation[][] annotations) {
+        annotations.findAll {validParameter(it)}
+                   .collect {handleParameters(it)}
+    }
+
+    def validParameter(Annotation[] annotations) {
+        annotations.find{it.annotationType() == ApiParam.class} != null
+    }
+
     def handleParameters(Annotation[] annotations)  {
         def result = [:]
+        ApiParam apiParam = annotations.find{it.annotationType() == ApiParam.class}
+        if (apiParam != null) {
+            if (apiParam.value()) result.description = apiParam.value()
+            if (apiParam.allowableValues()) result.allowableValues = apiParam.allowableValues()
+            result.name = apiParam.name()
+            result.required = apiParam.required()
+            result.allowMultiple = apiParam.allowMultiple()
+        }
         result
     }
+
+
 }
--- src/test/groovy/com/swagger/codegen/handlers/ParametersHandlerJaxrsSpec.groovy
+++ src/test/groovy/com/swagger/codegen/handlers/ParametersHandlerJaxrsSpec.groovy
@@ -1,24 +1,71 @@
 package com.swagger.codegen.handlers
 
 import com.swagger.docgen.handlers.ParametersHandler
+import com.wordnik.swagger.annotations.ApiParam
 import spock.lang.Specification
 
 class ParametersHandlerJaxrsSpec extends Specification {
 
     class ApiClass1 {
         def noAnnotationsMethod(String x) {}
-        def simpleMethod(String x) {}
+        def simpleMethod(@ApiParam(value = "description", name = "x") String x) {}
+        def advancedMethod(@ApiParam(value = "description", name = "x", required = true, allowableValues = "range[1,10]",
+                                     allowMultiple = true) String x) {}
+    }
+
+    def getParametersAnnotations(def methodName) {
+        ApiClass1.class.getMethods().find{it.name == methodName}.getParameterAnnotations()
+    }
+
+    def parametersHandler
+    def setup() {
+        parametersHandler = new ParametersHandler()
     }
 
     def "test method with no annotations"() {
         setup:
-            def parametersHandler = new ParametersHandler()
-            def parametersAnnotations = ApiClass1.class.getMethods().find{it.name =="noAnnotationsMethod"}.getParameterAnnotations()
+            def methodName = "noAnnotationsMethod"
+            def parametersAnnotations = getParametersAnnotations(methodName)
 
         when:
-            def result = parametersAnnotations.collect{ parametersHandler.handleParameters(it)}
+            def result = parametersHandler.handleParameters(parametersAnnotations)
         then:
-            println "Result is $result"
+            println "Result for method $methodName is $result"
             result != null
+    }
+
+    def "test simple method"() {
+        setup:
+            def methodName = "simpleMethod"
+            def parametersAnnotations = getParametersAnnotations(methodName)
+
+        when:
+            def result = parametersHandler.handleParameters(parametersAnnotations)
+        then:
+            println "Result for method $methodName is $result"
+            result != null
+            result.size() == 1
+            result[0].name == "x"
+            result[0].description == "description"
+            result[0].required == false
+            result[0].allowableValues == null
+            result[0].allowMultiple == false
+    }
+
+    def "test advanced method"() {
+        setup:
+            def methodName = "advancedMethod"
+            def parametersAnnotations = getParametersAnnotations(methodName)
+        when:
+            def result = parametersHandler.handleParameters(parametersAnnotations)
+        then:
+            println "Result for method $methodName is $result"
+            result != null
+            result.size() == 1
+            result[0].name == "x"
+            result[0].description == "description"
+            result[0].required == true
+            result[0].allowableValues == "range[1,10]"
+            result[0].allowMultiple == true
     }
 }
